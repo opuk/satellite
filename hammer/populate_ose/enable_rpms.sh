b5fec848ba660a6bbd287694f460a63d1e5b94fe
Johan Swensson <kupo@kupo.se>
2016-01-28 10:43:08 +0100

    Drop releasever from repos where it's not needed

--- hammer/populate_ose/enable_rpms.sh
+++ hammer/populate_ose/enable_rpms.sh
@@ -5,6 +5,6 @@ RELEASEVER=7Server
 
 #hammer repository-set list --product "Red Hat Enterprise Linux Server" --organization-id 1
 hammer repository-set enable --name "Red Hat Enterprise Linux 7 Server (RPMs)" --product "Red Hat Enterprise Linux Server" --organization-id $ORG_ID --basearch x86_64 --releasever $RELEASEVER
-hammer repository-set enable --name "Red Hat Satellite Tools 6.1 (for RHEL 7 Server) (RPMs)" --product "Red Hat Enterprise Linux Server" --organization-id $ORG_ID --basearch x86_64 --releasever $RELEASEVER
-hammer repository-set enable --name "Red Hat Enterprise Linux 7 Server - Extras (RPMs)" --product "Red Hat Enterprise Linux Server" --organization-id $ORG_ID --basearch x86_64 --releasever $RELEASEVER
-hammer repository-set enable --name "Red Hat OpenShift Enterprise 3.1 (RPMs)" --product "Red Hat OpenShift Enterprise" --organization-id $ORG_ID --basearch x86_64 --releasever $RELEASEVER
+hammer repository-set enable --name "Red Hat Satellite Tools 6.1 (for RHEL 7 Server) (RPMs)" --product "Red Hat Enterprise Linux Server" --organization-id $ORG_ID --basearch x86_64 
+hammer repository-set enable --name "Red Hat Enterprise Linux 7 Server - Extras (RPMs)" --product "Red Hat Enterprise Linux Server" --organization-id $ORG_ID --basearch x86_64 
+hammer repository-set enable --name "Red Hat OpenShift Enterprise 3.1 (RPMs)" --product "Red Hat OpenShift Enterprise" --organization-id $ORG_ID --basearch x86_64 
