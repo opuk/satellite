4c18159547b4df33f1d362e808c8066c0590a59a
Johan Swensson <kupo@kupo.se>
2016-02-01 17:01:52 +0100

    add kickstart tree

--- hammer/populate_ose/enable_rpms.sh
+++ hammer/populate_ose/enable_rpms.sh
@@ -4,6 +4,7 @@ ORG_ID=1
 RELEASEVER=7Server
 
 #hammer repository-set list --product "Red Hat Enterprise Linux Server" --organization-id 1
+hammer repository-set enable --name "Red Hat Enterprise Linux 7 Server (Kickstart)" --product "Red Hat Enterprise Linux Server" --organization-id $ORG_ID --basearch x86_64 --releasever 7Server
 hammer repository-set enable --name "Red Hat Enterprise Linux 7 Server (RPMs)" --product "Red Hat Enterprise Linux Server" --organization-id $ORG_ID --basearch x86_64 --releasever $RELEASEVER
 hammer repository-set enable --name "Red Hat Satellite Tools 6.1 (for RHEL 7 Server) (RPMs)" --product "Red Hat Enterprise Linux Server" --organization-id $ORG_ID --basearch x86_64 
 hammer repository-set enable --name "Red Hat Enterprise Linux 7 Server - Extras (RPMs)" --product "Red Hat Enterprise Linux Server" --organization-id $ORG_ID --basearch x86_64 
