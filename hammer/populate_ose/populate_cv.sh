39f034a231d306d4c0c14665eb5d2afeb5dee647
Johan Swensson <kupo@kupo.se>
2016-02-01 16:18:39 +0100

    Add sync.sh, populate_cv.sh and README Rename populate.sh -> populate-docker.sh

---
+++ hammer/populate_ose/populate_cv.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+hammer content-view create --name "Openshift 3" --organization-id 1 
+hammer content-view add-repository --name "Openshift 3" --organization-id 1 --product "Red Hat Enterprise Linux Server" --repository "Red Hat Enterprise Linux 7 Server - Extras RPMs x86_64"
+hammer content-view add-repository --name "Openshift 3" --organization-id 1 --product "Red Hat Enterprise Linux Server" --repository "Red Hat Enterprise Linux 7 Server RPMs x86_64 7Server"
+hammer content-view add-repository --name "Openshift 3" --organization-id 1 --product "Red Hat Enterprise Linux Server" --repository "Red Hat Satellite Tools 6.1 for RHEL 7 Server RPMs x86_64"
+hammer content-view add-repository --name "Openshift 3" --organization-id 1 --product "Red Hat OpenShift Enterprise" --repository "Red Hat OpenShift Enterprise 3.1 RPMs x86_64"
+
