56fcf0628bbca7c809eb62b34e7f56cce04f783b
Johan Swensson <kupo@kupo.se>
2016-02-03 23:12:09 +0100

    Add initial setup script

---
+++ hammer/base_config/setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+CUSTOMER_NAME="Example INC"
+LOCATION="The Moon"
+DOMAIN="example.com"
+MANIFEST=/root/manifest.zip
+
+#Network configuration (first provision network)
+NETNAME=provnet1
+GATEWAY=192.168.122.1
+DNS1=192.168.122.1
+DNS2=4.2.2.2
+NETWORK=192.168.122.0
+NETMASK=255.255.255.0
+DHCPSTART=192.168.122.100
+DHCPSTOP=192.168.122.200
+
+# Create the $CUSTOMER_NAME Organisation
+hammer organization create --name "$CUSTOMER_NAME"
+
+# Create the $LOCATION Location, one of the datacenters
+hammer location create --name "$LOCATION"
+
+# Upload the Satellite Manifest
+hammer subscription upload --organization "$CUSTOMER_NAME" --file $MANIFEST
+
+# Add the domain, domain is usually created by katello-installer
+hammer domain create --name $DOMAIN
+
+# Create subnet
+hammer subnet create --dns-primary $DNS1 --dns-secondary $DNS2 --name $NETNAME --network "$NETWORK" --mask $NETMASK --gateway $GATEWAY --domains "$DOMAIN" --from $DHCPSTART --to $DHCPSTOP --dhcp-id 1 --tftp-id 1
+
+# Add domain, smart-proxy and subnet to organisation
+hammer organization add-domain --name "$CUSTOMER_NAME" --domain $DOMAIN
+#hammer organization add-smart-proxy --name "$CUSTOMER_NAME" --smart-proxy $SATELLITE
+hammer organization add-subnet --name "$CUSTOMER_NAME" --subnet $NETNAME
+
+# Add domain, smart-proxy and subnet to location
+hammer location add-domain --name "$LOCATION" --domain $DOMAIN
+#hammer location add-smart-proxy --name $LOCATION --smart-proxy rhs.$CUSTOMER_NAME.domain
+hammer location add-subnet --name "$LOCATION" --subnet $NETNAME
+
+#Create basic lifecycle environment
+hammer lifecycle-environment create --name Dev --prior Library --organization "$CUSTOMER_NAME"
+hammer lifecycle-environment create --name QA --prior Dev --organization "$CUSTOMER_NAME"
+hammer lifecycle-environment create --name Prod --prior QA --organization "$CUSTOMER_NAME"
+
