ff4ba24f8eab5694525dc96d00cef66e57a347cc
Johan Swensson <kupo@kupo.se>
2016-02-10 15:50:57 +0100

    escape for to make it easier to read

--- hammer/base_config/rhel7_cv.sh
+++ hammer/base_config/rhel7_cv.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 hammer content-view create --name "RHEL7" --organization-id 1
-hammer content-view add-repository --name "RHEL7" --organization-id 1 --product "Red Hat Enterprise Linux Server" --repository "Red Hat Enterprise Linux 7 Server - Extras RPMs x86_64"
-hammer content-view add-repository --name "RHEL7" --organization-id 1 --product "Red Hat Enterprise Linux Server" --repository "Red Hat Enterprise Linux 7 Server RPMs x86_64 7Server"
-hammer content-view add-repository --name "RHEL7" --organization-id 1 --product "Red Hat Enterprise Linux Server" --repository "Red Hat Enterprise Linux 7 Server - Optional (RPMs)"
-hammer content-view add-repository --name "RHEL7" --organization-id 1 --product "Red Hat Enterprise Linux Server" --repository "Red Hat Satellite Tools 6.1 for RHEL 7 Server RPMs x86_64"
+
+hammer content-view add-repository --name "RHEL7" --organization-id "1" \
+--product "Red Hat Enterprise Linux Server" \
+--repository "Red Hat Enterprise Linux 7 Server - Extras RPMs x86_64"
+
+hammer content-view add-repository --name "RHEL7" --organization-id "1" \
+--product "Red Hat Enterprise Linux Server" \
+--repository "Red Hat Enterprise Linux 7 Server RPMs x86_64 7Server"
+
+hammer content-view add-repository --name "RHEL7" --organization-id "1" \
+--product "Red Hat Enterprise Linux Server" \
+--repository "Red Hat Enterprise Linux 7 Server - Optional RPMs x86_64 7Server"
+
+hammer content-view add-repository --name "RHEL7" --organization-id "1" \
+--product "Red Hat Enterprise Linux Server" \
+--repository "Red Hat Satellite Tools 6.1 for RHEL 7 Server RPMs x86_64"
