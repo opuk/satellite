5b238b3d91b224b5a0fc7d68853facd300976c58
Johan Swensson <kupo@kupo.se>
2016-05-07 10:56:03 +0200

    add additional images to sync

--- hammer/populate_ose/populate-docker.sh
+++ hammer/populate_ose/populate-docker.sh
@@ -3,7 +3,7 @@
 ORG_ID=1
 PRODUCT_NAME="OSE Docker images"
 
-upstream_repos=( openshift3/ose-deployer openshift3/ose-docker-registry openshift3/ose-pod openshift3/ose-docker-builder openshift3/ose-sti-builder openshift3/ose-haproxy-router openshift3/mongodb-24-rhel7 openshift3/mysql-55-rhel7 openshift3/nodejs-010-rhel7 openshift3/perl-516-rhel7 openshift3/php-55-rhel7 openshift3/postgresql-92-rhel7 openshift3/python-33-rhel7 openshift3/ruby-20-rhel7 openshift3/logging-deployment openshift3/logging-elasticsearch openshift3/logging-kibana openshift3/logging-fluentd openshift3/logging-auth-proxy openshift3/metrics-deployer openshift3/metrics-hawkular-metrics openshift3/metrics-cassandra openshift3/metrics-heapster )
+upstream_repos=( openshift3/ose-deployer openshift3/ose-docker-registry openshift3/ose-pod openshift3/ose-docker-builder openshift3/ose-sti-builder openshift3/ose-haproxy-router openshift3/mongodb-24-rhel7 openshift3/mysql-55-rhel7 openshift3/nodejs-010-rhel7 openshift3/perl-516-rhel7 openshift3/php-55-rhel7 openshift3/postgresql-92-rhel7 openshift3/python-33-rhel7 openshift3/ruby-20-rhel7 openshift3/logging-deployment openshift3/logging-elasticsearch openshift3/logging-kibana openshift3/logging-fluentd openshift3/logging-auth-proxy openshift3/metrics-deployer openshift3/metrics-hawkular-metrics openshift3/metrics-cassandra openshift3/metrics-heapster openshift3/jenkins-1-rhel7 openshift3/image-inspector openshift3/ose-recycler )
 
 hammer product create --name "$PRODUCT_NAME" --organization-id $ORG_ID
 
