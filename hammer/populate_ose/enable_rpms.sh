75ce437322abc65eef59cee2c52884ab664d3141
Johan Swensson <kupo@kupo.se>
2016-08-02 16:50:21 +0200

    bump versions

--- hammer/populate_ose/enable_rpms.sh
+++ hammer/populate_ose/enable_rpms.sh
@@ -6,6 +6,6 @@ RELEASEVER=7Server
 #hammer repository-set list --product "Red Hat Enterprise Linux Server" --organization-id 1
 hammer repository-set enable --name "Red Hat Enterprise Linux 7 Server (Kickstart)" --product "Red Hat Enterprise Linux Server" --organization-id $ORG_ID --basearch x86_64 --releasever 7Server
 hammer repository-set enable --name "Red Hat Enterprise Linux 7 Server (RPMs)" --product "Red Hat Enterprise Linux Server" --organization-id $ORG_ID --basearch x86_64 --releasever $RELEASEVER
-hammer repository-set enable --name "Red Hat Satellite Tools 6.1 (for RHEL 7 Server) (RPMs)" --product "Red Hat Enterprise Linux Server" --organization-id $ORG_ID --basearch x86_64 
+hammer repository-set enable --name "Red Hat Satellite Tools 6.2 (for RHEL 7 Server) (RPMs)" --product "Red Hat Enterprise Linux Server" --organization-id $ORG_ID --basearch x86_64 
 hammer repository-set enable --name "Red Hat Enterprise Linux 7 Server - Extras (RPMs)" --product "Red Hat Enterprise Linux Server" --organization-id $ORG_ID --basearch x86_64 
-hammer repository-set enable --name "Red Hat OpenShift Enterprise 3.1 (RPMs)" --product "Red Hat OpenShift Enterprise" --organization-id $ORG_ID --basearch x86_64 
+hammer repository-set enable --name "Red Hat OpenShift Enterprise 3.2 (RPMs)" --product "Red Hat OpenShift Enterprise" --organization-id $ORG_ID --basearch x86_64 
--- hammer/populate_ose/populate_cv.sh
+++ hammer/populate_ose/populate_cv.sh
@@ -11,10 +11,10 @@ hammer content-view add-repository --name "Openshift 3" \
 
 hammer content-view add-repository --name "Openshift 3" --organization-id "1" \
 --product "Red Hat Enterprise Linux Server" \
---repository "Red Hat Satellite Tools 6.1 for RHEL 7 Server RPMs x86_64"
+--repository "Red Hat Satellite Tools 6.2 for RHEL 7 Server RPMs x86_64"
 
 hammer content-view add-repository --name "Openshift 3" --organization-id "1" \
 --product "Red Hat OpenShift Enterprise" \
---repository "Red Hat OpenShift Enterprise 3.1 RPMs x86_64"
+--repository "Red Hat OpenShift Enterprise 3.2 RPMs x86_64"
 
 
