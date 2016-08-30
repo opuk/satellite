a70c9409952800829c074295f5663ea9f5209a60
Johan Swensson <kupo@kupo.se>
2016-08-30 13:22:49 +0200

    drop releasever

--- hammer/cloudforms/enable_cloudforms_rpms.sh
+++ hammer/cloudforms/enable_cloudforms_rpms.sh
@@ -6,5 +6,5 @@ RELEASEVER=7Server
 #hammer repository-set list --product "Red Hat Enterprise Linux Server" --organization-id 1
 hammer repository-set enable --name "Red Hat Enterprise Linux 7 Server (RPMs)" --product "Red Hat Enterprise Linux Server" --organization-id $ORG_ID --basearch x86_64 --releasever $RELEASEVER
 hammer repository-set enable --name "Red Hat Satellite Tools 6.2 (for RHEL 7 Server) (RPMs)" --product "Red Hat Enterprise Linux Server" --organization-id $ORG_ID --basearch x86_64 
-hammer repository-set enable --name "Red Hat CloudForms Management Engine 5.6 (RPMs)" --product "Red Hat CloudForms" --organization-id $ORG_ID --basearch x86_64 --releasever $RELEASEVER
+hammer repository-set enable --name "Red Hat CloudForms Management Engine 5.6 (RPMs)" --product "Red Hat CloudForms" --organization-id $ORG_ID --basearch x86_64 
 hammer repository-set enable --name "Red Hat Software Collections RPMs for Red Hat Enterprise Linux 7 Server" --product "Red Hat Software Collections for RHEL Server" --organization-id $ORG_ID --basearch x86_64 --releasever $RELEASEVER
