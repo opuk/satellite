65cf9f46909ee634b72d8ccd33155fe8c2e7d21b
Johan Swensson <johan@redhat.com>
2016-09-30 11:32:35 +0200

    bump versions

--- hammer/capsule/enable_capsule.sh
+++ hammer/capsule/enable_capsule.sh
@@ -4,7 +4,7 @@ ORG_ID=1
 RELEASEVER=7Server
 
 #hammer repository-set list --product "Red Hat Enterprise Linux Server" --organization-id 1
-hammer repository-set enable --name "Red Hat Satellite Capsule 6.1 (for RHEL 7 Server) (RPMs)" --product "Red Hat Satellite Capsule" --organization-id $ORG_ID --basearch x86_64
+hammer repository-set enable --name "Red Hat Satellite Capsule 6.2 (for RHEL 7 Server) (RPMs)" --product "Red Hat Satellite Capsule" --organization-id $ORG_ID --basearch x86_64
 
 
 
--- hammer/populate_ose/enable_rpms.sh
+++ hammer/populate_ose/enable_rpms.sh
@@ -8,4 +8,4 @@ hammer repository-set enable --name "Red Hat Enterprise Linux 7 Server (Kickstar
 hammer repository-set enable --name "Red Hat Enterprise Linux 7 Server (RPMs)" --product "Red Hat Enterprise Linux Server" --organization-id $ORG_ID --basearch x86_64 --releasever $RELEASEVER
 hammer repository-set enable --name "Red Hat Satellite Tools 6.2 (for RHEL 7 Server) (RPMs)" --product "Red Hat Enterprise Linux Server" --organization-id $ORG_ID --basearch x86_64 
 hammer repository-set enable --name "Red Hat Enterprise Linux 7 Server - Extras (RPMs)" --product "Red Hat Enterprise Linux Server" --organization-id $ORG_ID --basearch x86_64 
-hammer repository-set enable --name "Red Hat OpenShift Enterprise 3.2 (RPMs)" --product "Red Hat OpenShift Enterprise" --organization-id $ORG_ID --basearch x86_64 
+hammer repository-set enable --name "Red Hat OpenShift Enterprise 3.2 (RPMs)" --product "Red Hat OpenShift Container Platform" --organization-id $ORG_ID --basearch x86_64 
