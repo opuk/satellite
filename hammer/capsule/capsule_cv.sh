2c9142f3b0d655e9ed12881538e08a222715a225
Johan Swensson <johan@redhat.com>
2016-09-30 19:18:45 +0200

    bump versions

--- hammer/capsule/capsule_cv.sh
+++ hammer/capsule/capsule_cv.sh
@@ -7,8 +7,8 @@ hammer content-view add-repository --name "Capsule" --organization-id "1" \
 
 hammer content-view add-repository --name "Capsule" --organization-id "1" \
 --product "Red Hat Satellite Capsule" \
---repository "Red Hat Satellite Capsule 6.1 for RHEL 7 Server RPMs x86_64"
+--repository "Red Hat Satellite Capsule 6.2 for RHEL 7 Server RPMs x86_64"
 
 hammer content-view add-repository --name "Capsule" --organization-id "1" \
 --product "Red Hat Enterprise Linux Server" \
---repository "Red Hat Satellite Tools 6.1 for RHEL 7 Server RPMs x86_64"
+--repository "Red Hat Satellite Tools 6.2 for RHEL 7 Server RPMs x86_64"
