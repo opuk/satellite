7988787276c91cef572a61185150889eab0594b3
Johan Swensson <kupo@kupo.se>
2017-04-03 20:28:23 +0200

    fix urls

--- hammer/populate_ose/standalone-registry.sh
+++ hammer/populate_ose/standalone-registry.sh
@@ -3,14 +3,14 @@
 ORG_ID=1
 PRODUCT_NAME="OCP Docker images"
 
-upstream_repos=( registry.access.redhat.com/openshift3/node \
-registry.access.redhat.com/openshift3/ose-haproxy-router \
-registry.access.redhat.com/openshift3/ose-deployer \
-registry.access.redhat.com/openshift3/openvswitch \
-registry.access.redhat.com/openshift3/ose-docker-registry \
-registry.access.redhat.com/openshift3/ose \
-registry.access.redhat.com/openshift3/ose-pod \
-registry.access.redhat.com/openshift3/registry-console
+upstream_repos=( openshift3/node \
+openshift3/ose-haproxy-router \
+openshift3/ose-deployer \
+openshift3/openvswitch \
+openshift3/ose-docker-registry \
+openshift3/ose \
+openshift3/ose-pod \
+openshift3/registry-console
 )
 
 hammer product create --name "$PRODUCT_NAME" --organization-id $ORG_ID
